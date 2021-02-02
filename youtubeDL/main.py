524291514a2b393d03cdedd5c5b38cf7fd8d336b
ahanna334 <78327872+ahanna334@users.noreply.github.com>
2021-02-01 21:30:44 -0800

    Add files via upload

--- youtubeDL/main.py
+++ youtubeDL/main.py
@@ -18,8 +18,10 @@ if len(os.listdir('./static')) == 0:
       if file.endswith(".mp3"):
         var = os.path.join("./static", file)
         print(var)
+        N = 9
+        res = var[:N] + var[N].upper() + var [N +1:]
        
-        okay = var.strip('.\/static\\')
+        okay = res.strip('.\/static\\')
         print(okay)
         encode = okay.encode('ascii',errors='ignore')
         decode = encode.decode()
@@ -42,18 +44,20 @@ if len(os.listdir('./static')) == 0:
 else:    
     print("Directory is not empty")
     files = glob.glob('./static/*.mp3')
-for f in files:
-    os.remove(f)
-call(['youtube-dl', '--restrict-filenames', '-ciw', '-o', '/static/%(title)s.%(ext)s', '-x', '--audio-format', 'mp3', '-a', './URL.txt'], shell=False)
-open('URL.txt', 'w').close()
-time.sleep(10)
+    for f in files:
+      os.remove(f)
+      call(['youtube-dl', '--restrict-filenames', '-ciw', '-o', '/static/%(title)s.%(ext)s', '-x', '--audio-format', 'mp3', '-a', './URL.txt'], shell=False)
+      open('URL.txt', 'w').close()
+      time.sleep(10)
 
-for file in os.listdir("./static"):
+    for file in os.listdir("./static"):
       if file.endswith(".mp3"):
         var = os.path.join("./static", file)
         print(var)
+        N = 9
+        res = var[:N] + var[N].upper() + var[N + 1:]
        
-        okay = var.strip('.\/static\\')
+        okay = res.strip('.\/static\\')
         print(okay)
         encode = okay.encode('ascii',errors='ignore')
         decode = encode.decode()
@@ -63,11 +67,11 @@ for file in os.listdir("./static"):
         os.rename("./static/" + okay, "./static/" + decode)
         shutil.copy('./templates/default.txt', './templates/test.html')
         time.sleep(1)
-with open('./templates/test.html','r') as f:
+    with open('./templates/test.html','r') as f:
         newlines = []
         for line in f.readlines():
           newlines.append(line.replace('mySong.mp3', decode))
-with open('./templates/test.html', 'w') as f:
+    with open('./templates/test.html', 'w') as f:
         for line in newlines:
           f.write(line)
 
